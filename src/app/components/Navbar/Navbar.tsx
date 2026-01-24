3d77c721dfba0939e6a555f169c72594350001de
RafalSprengel <sprengel.rafal@gmail.com>
2026-01-24 14:17:09 +0000

    changes

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -34,10 +34,10 @@ export default function Navbar() {
                     <div className={styles.mobileMenuInner}>
                         <ul className={`${styles.navLinks} ${isMenuOpen ? styles.showMenu : ''}`}>
                             <li><Link href="/">STRONA GŁÓWNA</Link></li>
-                            <li><Link href="#services">OFERTA</Link></li>
+                            <li><Link href="/#services">OFERTA</Link></li>
                             <li><Link href="/gallery">GALERIA</Link></li>
-                            <li><Link href="#attractions">KASZUBY</Link></li>
-                            <li><Link href="#contact">KONTAKT</Link></li>
+                            <li><Link href="/#attractions">KASZUBY</Link></li>
+                            <li><Link href="/#contact">KONTAKT</Link></li>
                         </ul>
                     </div>
                 </div>
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function Gallery() {
+    return (
+        <div className={styles.galleryContainer}>
+            <div className={styles.galleryContent}>Tu będzie galeria :)</div>
+        </div>
+    );
+
+}
