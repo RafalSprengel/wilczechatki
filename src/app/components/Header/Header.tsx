89aa37e4005a26d08173662c1c17a8bfbd761649
RafalSprengel <sprengel.rafal@gmail.com>
2026-01-24 19:19:26 +0000

    changes

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,26 @@
+'use client'
+import { useState, useEffect, use } from 'react';
 import TopBar from '@components/Navbar/TopBar';
 import Navbar from '@components/Navbar/Navbar';
+import styles from './Header.module.css';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const scrollY = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }
+        window.addEventListener('scroll', scrollY);
+        return ()=> window.removeEventListener('scroll', scrollY);
+    }, [])
+
     return (
-        <header>
+        <header className={styles.container + (isScrolled ? ' ' + styles.scrolled : '')}>
             <TopBar />
             <Navbar />
         </header>
