11b3a74e00b06597dccbf34b133812de2b4355b4
Rafal Sprengel <sprengel.rafal@gmail.com>
2026-04-11 21:32:03 +0100

    all properties

---
+++ src/app/(root)/booking/allPropertiesCard.tsx
@@ -0,0 +1,8 @@
+export default function AllPropertiesCard() {
+  return (
+    <div >
+      <h2 className="text-xl font-semibold mb-2">Wszystkie domki</h2>
+      <p className="text-gray-600">Zobacz wszystkie dostępne domki i zarządzaj nimi.</p>
+    </div>
+  );
+}
